a99521bd8b9ae63c5920c48f931eb2379992b883
Bastien Mazeran <bastien.mazeran@autodesk.com>
2017-11-02 15:56:30 -0700

    Create start.js

---
+++ start.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var app = require('./server/server');
+
+// start server
+var server = app.listen(app.get('port'), function () {
+  if (process.env.FORGE_CLIENT_ID == null || process.env.FORGE_CLIENT_SECRET == null)
+    console.log('*****************\nWARNING: Forge Client ID & Client Secret not defined as environment variables.\n*****************');
+
+  console.log('Starting at ' + (new Date()).toString());
+  console.log('Server listening on port ' + server.address().port);
+});
